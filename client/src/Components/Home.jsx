bf5382fcf05966fa6e1c8e45897831fca2323697
Sai Prashanth <saiprashanth754@gmail.com>
2025-01-29 14:51:22 +0530

    refactor: simplify App component and add Home component

--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,6 +1,7 @@
 import { motion } from "framer-motion";
+import React from "react";
 
-function Home() {
+const Home = () =>{
     return (
         <motion.div
             className="page-container"
