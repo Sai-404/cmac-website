cf8ea9e340319d180f788c095f5df1ddeb984c3d
Sai Prashanth <saiprashanth754@gmail.com>
2025-02-01 15:35:36 +0530

    feat: add Footer component

---
+++ client/src/Components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+
+    return (
+        <footer className="bg-gray-800 text-white py-12">
+        <div className="max-w-4xl mx-auto text-center">
+            <p className="text-sm">
+            &copy; 2021 SRMIST CMAC. All rights reserved.
+            </p>
+        </div>
+        </footer>
+    );
+}
+
+
+export default Footer;
--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -3,12 +3,14 @@ import React from "react";
 import Header from "./Header.jsx";
 import Hero from "./Hero.jsx";
 import '../index.css';
+import Footer from "./Footer.jsx";
 
 const Home = () => {
   return (
    <div className="w-screen min-h-screen bg-gray-100">
     <Header/>
     <Hero/>
+    <Footer/>
    </div>
   );
 };
