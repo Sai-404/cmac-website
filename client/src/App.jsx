3b0c13bf2a3857aa54dcfddd7982122e483fe058
anirudhanalwar <sreeanirudhanalwar@gmail.com>
2025-02-12 18:35:01 +0530

    added redirecting page for contact

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import { Routes, Route } from 'react-router-dom' // Add these imports
 import Home from './Home/Home.jsx'
 import About from './About/About.jsx' // Add this import if you have an About page
-
+import Contact from './Contact/Contact.jsx'
 function App() {
   return (
       <Routes>
         <Route path="/" element={<Home />} /> {/* Home page */}
         <Route path="/about" element={<About />} /> {/* About page */}
+        <Route path="/contact" element={<Contact />} /> {/* contact page */}
       </Routes>
   );
 }
